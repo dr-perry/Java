a835700a93fe8012dfb60a0b463ad3ddfdb7b45e
Hayden Perry <perry.01@hotmail.co.uk>
2013-01-20 17:37:30 +0000

    update

--- src/snake/shape.java
+++ src/snake/shape.java
@@ -2,6 +2,14 @@ package snake;
 
 public class shape {
 
+	int square_size = 10;
+	int item_size = 10;
+	int gap = 2;
+	int speed = 100;
+	int rand_pos = 30;
+	int max_squares = 1000;
 	
+	int x[] = new int[max_squares];
+	int y[] = new int[max_squares];
 
 }
--- src/snake/snake.java
+++ src/snake/snake.java
@@ -1,14 +1,37 @@
 package snake;
+import java.awt.BorderLayout;
+
 import javax.swing.*;
 
+@SuppressWarnings("serial")
 public class snake extends JFrame {
 	
-	int score = 0;
-	JLabel scoreBar = new JLabel(String score);
+	static JLabel scoreBar = new JLabel();
+	
+	public static void score() {
+		
+		int newScore = 5;
+		scoreBar.setText(String.valueOf(newScore));
+		
+	}
+	
+	public snake() {
+		
+		setVisible(true);
+		setSize(500, 500);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Snake");
+		setLocationRelativeTo(null);
+		
+		add(scoreBar, BorderLayout.SOUTH);
+		
+	}
 	
 	public static void main(String[] args) {
 		
-		
+		new snake();
+		score();
 		
 	}
 
